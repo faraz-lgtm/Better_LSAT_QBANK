3c26128379f4b3b85cdbc62167472a3fc85f5f87
assad-siyal <assad@scalebrands.ca>
2026-09-24 00:01:57 +0500

    changes done

--- web/src/features/student/pages/rc-new-drill-page.tsx
+++ web/src/features/student/pages/rc-new-drill-page.tsx
@@ -35,7 +35,7 @@ function RcNewDrillPage() {
   }, [analyticsApi])
 
   return (
-    <StudentMain className="bg-[var(--background)]" contentClassName="bg-[var(--background)]">
+    <StudentMain className="bg-[var(--primary-0)]" contentClassName="bg-[var(--primary-0)]">
       <DrillConfigForm
         sectionType="RC"
         initialQuestionTypeId={questionTypeId}
